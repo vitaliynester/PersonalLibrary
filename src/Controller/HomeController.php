defdabb4ec83956d838ce8b95a3a3a52efea5410
Vitaly Posadnev <elemabor@gmail.com>
2021-05-03 22:55:03 +0300

    add comment for HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,23 +12,32 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
+     * Обработчик главной страницы
+     *
      * @Route("/", name="home")
+     *
      * @param BookRepository $bookRepository
      * @param PaginatorInterface $paginator
      * @param Request $request
+     *
      * @return Response
      */
     public function index(BookRepository $bookRepository, PaginatorInterface $paginator, Request $request): Response
     {
+        // Получаем список всех книг с сортировкой по дате прочтения
         $books = $bookRepository->findBy(
             [],
             ['readDate' => 'DESC'],
         );
+
+        // Получаем разбивку по страницам для всех книг
         $pagination = $paginator->paginate(
             $books,
             $request->query->getInt('page', 1),
             12
         );
+
+        // Передаем в шаблон список книг на странице
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'books' => $pagination,
